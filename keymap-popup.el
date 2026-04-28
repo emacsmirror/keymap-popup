7aa36cdff46f46334299e839c64e94a5d39999bf
Thanos Apollo <public@thanosapollo.org>
2026-04-29 02:20:48 +0300

    packaging: Version bump 0.2.2
    * Should have been included in b8012d7

--- keymap-popup.el
+++ keymap-popup.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2026  Free Software Foundation, Inc.
 
 ;; Author: Thanos Apollo <public@thanosapollo.org>
-;; Version: 0.2.1
+;; Version: 0.2.2
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: convenience
 ;; URL: https://codeberg.org/thanosapollo/emacs-keymap-popup
